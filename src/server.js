e97f6de713ccf1bbb1ecdb470c35e68cee7764f9
romuloalex <romulobier@gmail.com>
2025-10-27 12:15:14 -0300

    criação do server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+import http from "node:http"
+
+const server = http.createServer((request, response) => {
+  return response.end("Hello World!")
+})
+
+server.listen(3333)
