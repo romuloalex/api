1873f0cfbc00fcf034555fa41c4cf9b5f2516ef1
romuloalex <romulobier@gmail.com>
2025-10-27 13:10:33 -0300

    mudança na console

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import http from "node:http"
 
 const server = http.createServer((request, response) => {
-  return response.end("Hello World!")
+  return response.end("Sucesso World!")
 })
 
 server.listen(3333)
