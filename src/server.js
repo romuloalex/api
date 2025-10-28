82f3459032ada9779fb365be79064f05f4b96506
romuloalex <romulobier@gmail.com>
2025-10-28 09:09:02 -0300

    mudanças insomnia

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 import http from "node:http"
 
 const server = http.createServer((request, response) => {
-  return response.end("Sucesso World!")
+  const { method, url } = request
+
+  if (method === "GET" && url === "/products") {
+    return response.end("Lista de produtos!")
+  }
+
+  if (method === "POST" && url === "/products") {
+    return response.writeHead(201).end("Produto cadastrado!")
+  }
+
+  return response.writeHead(404).end("Rota não encontrada!")
 })
 
 server.listen(3333)
