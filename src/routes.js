7583d5b3111ea87aeb53729b49731951aae21682
romuloalex <romulobier@gmail.com>
2025-10-30 08:06:00 -0300

    database

--- src/routes.js
+++ src/routes.js
@@ -1,29 +1,36 @@
-import { parseRoutePath } from "./utils/parseRoutePath.js"
+import { parseRoutePath } from "./utils/parseRoutePath.js";
 
 export const routes = [
   {
     method: "GET",
     path: "/products",
-    controller: (request, response) => {
-        return response.end(JSON.stringify(request.query))
+    controller: ({ request, response, database }) => {
+      const products = database.select("products");
+
+      return response.end(JSON.stringify(products));
     },
   },
+
   {
     method: "POST",
     path: "/products",
-    controller: (request, response) => {
-      return response.writeHead(201).end(JSON.stringify(request.body))
+    controller: ({ request, response, database }) => {
+      const { name, price } = request.body;
+
+      database.insert("products", { name, price });
+
+      return response.writeHead(201).end();
     },
   },
+
   {
-  method: "DELETE",
-  path: "/products/:id",
-  controller: (request, response) => {
-    return response.end("Produto removido com ID: " + request.params.id)
+    method: "DELETE",
+    path: "/products/:id",
+    controller: ({ request, response }) => {
+      return response.end("Produto removido com ID: " + request.params.id);
+    },
   },
-},
 ].map((route) => ({
   ...route,
   path: parseRoutePath(route.path),
-}))
-
+}));
